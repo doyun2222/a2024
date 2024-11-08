818f2696f74f572c3368344c267bc0eb2772592a
doyun2222 <vividsu1@naver.com>
2024-11-08 23:39:06 +0900

    Update script.js

--- portfoliosite/script.js
+++ portfoliosite/script.js
@@ -1,10 +1,9 @@
-// 상단 고정 배너 크기 조절
 const banner = document.querySelector('.portfolio-banner');
 
 window.addEventListener('scroll', () => {
     if (window.scrollY > 0) {
-        banner.classList.add('shrink'); // 스크롤 시 배너 축소
+        banner.classList.add('shrink');
     } else {
-        banner.classList.remove('shrink'); // 최상단으로 돌아오면 원래 크기로
+        banner.classList.remove('shrink');
     }
 });
